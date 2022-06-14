3a8cc56a3e9246bc82853b7e1f1426888d726572
Patricia Robles <patriciarobles9@gmail.com>
2022-06-14 16:23:46 +0100

    random color button added

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,21 @@
-document.addEventListener("DOMContentLoaded", function () {
-  // Add your JavaScript here.
+// document.addEventListener("DOMContentLoaded", function () {
+//   // Add your JavaScript here.
+// });
+
+const posts = document.getElementsByClassName("individual-posts");
+
+document.getElementById("color-change").addEventListener("click", function () {
+  for (let individualPost in posts) {
+    let randomColor = `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(
+      Math.random() * 255
+    )}, ${Math.floor(Math.random() * 255)})`;
+    posts[individualPost].style["background-color"] = randomColor;
+  }
+});
+
+document.getElementById("normal-color").addEventListener("click", function () {
+  for (let individualPost in posts)
+    if (posts[individualPost].style["background-color"] !== "#c9c9c9") {
+      posts[individualPost].style["background-color"] = "#c9c9c9";
+    }
 });
